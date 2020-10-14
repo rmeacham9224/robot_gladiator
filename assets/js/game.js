a505a87d22625b214acbb2fc82f127654c811952
rmeacham9224 <rcm0025@auburn.edu>
2020-10-13 21:23:24 -0500

    robot name

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,11 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt ("What is your robots name?");
+console.log(playerName);
+console.log("This logs a string, good for leaving you a message");
+//this will do math and log 20 
+console.log(10 +10);
+//what is this?
+console.log("our robots name is" + playerName)
+function fight() {
+    window.alert ("the fight has begun!");
+}
+// fight();
