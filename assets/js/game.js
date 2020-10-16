c23dfec85562824b62152b96c371daaefaf78286
rmeacham9224 <rcm0025@auburn.edu>
2020-10-16 17:10:06 -0500

    high score

--- assets/js/game.js
+++ assets/js/game.js
@@ -110,13 +110,23 @@ var startGame = function() {
 };
 
 var endGame = function() {
-    if (playerInfo.health > 0) {
-        window.alert("Great Job, you survived the game! You now have a score of " + playerInfo.money + ".");
-    }
-    else {
-        window.alert("You've lost your robot in battle.");
-    }
+        window.alert("The game has now ended. Let's see how you did!");
 
+        var highScore = localStorage.getItem("highscore");
+        if (highScore === null){
+            highScore = 0;
+        }
+
+        if (playerInfo.money > highScore) {
+            localStorage.setItem("highscore", playerInfo.money);
+            localStorage.setItem("name", playerInfo.name);
+
+            alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+        }
+        else {
+            alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time LOSER!");
+        }
+    
     
     var playAgainConfirm = window.confirm("Would you like to play again?");
 
